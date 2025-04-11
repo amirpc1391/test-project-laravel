189c7681dead132a7e74a106149f31b1c61fc632
amirpc <amirpc1380@gmal.com>
2025-04-12 02:49:59 +0330

    edited index.blade.php

--- resources/views/panel/users/index.blade.php
+++ resources/views/panel/users/index.blade.php
@@ -9,7 +9,7 @@
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="{{ route('users.index') }}">Home</a></li>
-                    <li class="breadcrumb-item active">Simple Tables</li>
+                    <li class="breadcrumb-item active">Users Tables</li>
                 </ol>
             </div>
         </div>
