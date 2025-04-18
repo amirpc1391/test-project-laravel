3931331c2c9356d0a2f0f65c6c9ce6c7c56afb9a
amirpc <amirpc1380@gmal.com>
2025-04-19 02:30:38 +0330

    root routes added

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ use App\Http\Controllers\AuthController;
 // use App\Http\Controllers\Auth;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+ Route::get('/', function () {
+     return view('welcome');
+ });
 
 
 // Route::get('/', function () {
